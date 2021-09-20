71f1fa4cad42c7086d53ab7209153e3ea5339b53
Sachintha Sampath <sachinsampath42@gmail.com>
2021-09-20 21:54:02 +0530

    First commit

--- src/NewCounterHooks.js
+++ src/NewCounterHooks.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState,useContext } from "react";
+import { ThemeContext } from "./App";
 
 export default function NewCounterHooks({ init }) {
     console.log("Render Counter Hooks");
     const [state, setState] = useState({ count: init });
     const [count,setCount] = useState(init);
+    const style = useContext(ThemeContext);
     return (
         <div>
-            <button onClick={() => setCount(ps=>ps-1)}>-</button>
+            <button style={style} onClick={() => setCount(ps=>ps-1)}>-</button>
             <span>{count}</span>
             <button onClick={() =>setCount(ps=>ps+1)}>+</button>
         </div>
